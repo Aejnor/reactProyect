092152835cbd9a6b4725115e3d80c85f33c34b68
Adolfo Borrego <borregoadolfo1@gmail.com>
2019-11-10 23:34:24 +0100

    Button added to retrieve data from pokeapi

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
 import logo from './logo.svg';
+import API from './utils/API';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button variant="success" onClick={dittoStats}>Ditto's Stats</Button>
       </header>
     </div>
   );
 }
+
+
+async function dittoStats(){
+  let stats = await API.get('/', {
+    params: {
+      results:1
+    }
+  });
+  stats = stats.data;
+  console.log(stats)
+
+};
 
 export default App;
