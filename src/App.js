f139883bb8373376bb9dd3f36ae9e9ac9bc4c65b
Adolfo Borrego <borregoadolfo1@gmail.com>
2019-11-11 19:39:22 +0100

    Not Working

--- src/App.js
+++ src/App.js
@@ -1,30 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Button from 'react-bootstrap/Button';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import API from './utils/API';
+// import axios from 'axios'
 import './App.css';
 
 
 function App() {
+  const [data, setData] = useState([ ]);
+
+    async function stats() {
+      let response = await API.get('/', {
+        params: {
+          results: 1
+        }
+      });
+      setData(response.data);
+      console.log(data);
+    }
+
+    useEffect(() => {
+      stats();
+    }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <Button variant="success" onClick={dittoStats}>Ditto's Stats</Button>
+        <Button variant="success"></Button>
       </header>
     </div>
   );
 }
-
-
-async function dittoStats(){
-  let stats = await API.get('/', {
-    params: {
-      results:1
-    }
-  });
-  stats = stats.data;
-  console.log(stats)
-
-};
 
 export default App;
